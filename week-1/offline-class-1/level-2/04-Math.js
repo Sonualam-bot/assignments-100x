6498f2145325c81c01c519ce6f38dc98957bb9ca
Sonualam <sonualam1599@gmail.com>
2023-12-13 12:49:05 +0530

    math comments

--- week-1/offline-class-1/level-2/04-Math.js
+++ week-1/offline-class-1/level-2/04-Math.js
@@ -1,27 +1,35 @@
 function mathMethods(value) {
   console.log("Original Value:", value);
 
+  // Math.round(): Rounds a number to the nearest integer.
   let rounded = Math.round(value);
   console.log("After round():", rounded);
 
+  // Math.ceil(): Rounds a number up to the nearest integer.
   let ceiling = Math.ceil(value);
   console.log("After ceil():", ceiling);
 
+  // Math.floor(): Rounds a number down to the nearest integer.
   let flooring = Math.floor(value);
   console.log("After floor():", flooring);
 
+  // Math.random(): Generates a random floating-point number between 0 and 1.
   let randomValue = Math.random();
   console.log("After random():", randomValue);
 
+  // Math.max(): Returns the highest value among the provided arguments.
   let maxValue = Math.max(5, 10, 15);
   console.log("After max():", maxValue);
 
+  // Math.min(): Returns the lowest value among the provided arguments.
   let minValue = Math.min(5, 10, 15);
   console.log("After min():", minValue);
 
+  // Math.pow(): Raises a number to the power of another.
   let powerOfTwo = Math.pow(value, 2);
   console.log("After pow():", powerOfTwo);
 
+  // Math.sqrt(): Calculates the square root of a number.
   let squareRoot = Math.sqrt(value);
   console.log("After sqrt():", squareRoot);
 }
